0caa3b93bd8bfd1167305001030075d18db7f002
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-02-26 16:07:08 +0200

    initial structure

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+// middleware
+
+const port = 5000;
+
+app.get('/hello',(req,res)=>{res.send('task manager app')})
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

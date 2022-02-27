bd8f93b9d756036965fa6f1ad5cd2cda3c3477f6
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-02-27 20:22:58 +0200

    auto formatted

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,14 +2,13 @@ const readFile = require("../utils/read");
 const write = require("../utils/write");
 
 const getAllTasks = async (req, res) => {
- try {
-  const data = readFile();
-  res.status(200).send(data); 
- } catch (error) {
-   console.log(error)
-  res.status(400).send(error);
- }
-  
+  try {
+    const data = readFile();
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 };
 
 const createTask = async (req, res) => {
@@ -21,7 +20,7 @@ const createTask = async (req, res) => {
     };
     const newarray = [...read, task];
     const writeFile = write(newarray);
-    res.status(200).send("Task created"); 
+    res.status(200).send("Task created");
   } catch (error) {
     res.status(400).send(error);
   }
@@ -37,17 +36,15 @@ const deleteTask = async (req, res, next) => {
   try {
     const data = readFile();
     let isFound = false;
-    let filtered = data.filter((task)=> {
-      isFound = task.id === req.params.id
+    let filtered = data.filter((task) => {
+      isFound = task.id === req.params.id;
       return task.id != req.params.id;
     });
-    
+
     const writeFile = write(filtered);
     res.status(200).send("delete Task");
-    
   } catch (error) {
     res.status(500).send(error);
-
   }
 };
 
@@ -57,18 +54,17 @@ const updateTask = async (req, res, next) => {
     const id = req.params.id;
     let isFound = false;
     const title = req.body.title;
-    const description=req.body.description
+    const description = req.body.description;
     const newArr = data.map((task) => {
       if (task.id === id) {
         isFound = true;
-        return { ...task, title ,description};
+        return { ...task, title, description };
       }
       return task;
     });
     if (isFound) {
       const writeFile = write(newArr);
       res.send("update Task");
-
     } else {
       res.status(404).send("not found");
     }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
   getAllTasks,
   createTask,
   updateTask,
   deleteTask,
   clearAllTasks,
-} = require('../controllers/tasks')
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask).delete(clearAllTasks)
-router.route('/:id').put(updateTask).delete(deleteTask)
-module.exports = router
+router.route("/").get(getAllTasks).post(createTask).delete(clearAllTasks);
+router.route("/:id").put(updateTask).delete(deleteTask);
+module.exports = router;
